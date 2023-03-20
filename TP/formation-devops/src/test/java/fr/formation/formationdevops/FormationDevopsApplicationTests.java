788ede26aa0eac559ca7c46fbd1454f4481d9dfa
Jeremy <jeremy.perrouault@ascadis.fr>
2023-03-20 12:28:31 +0100

    JOUR 1 - DEMO TEST UNITAIRE

--- TP/formation-devops/src/test/java/fr/formation/formationdevops/FormationDevopsApplicationTests.java
+++ TP/formation-devops/src/test/java/fr/formation/formationdevops/FormationDevopsApplicationTests.java
@@ -1,13 +1,22 @@
 package fr.formation.formationdevops;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FormationDevopsApplicationTests {
-
 	@Test
 	void contextLoads() {
-	}
 
+		Calculatrice calc = new Calculatrice();
+
+		Assertions.assertEquals(11, calc.additionner(5, 6));
+		Assertions.assertEquals(1, calc.soustraire(6, 5));
+
+		// Dans un test, on fait des assertions
+		Assertions.assertTrue(true);
+
+		Assertions.assertEquals(10, 5 + 5);
+	}
 }
